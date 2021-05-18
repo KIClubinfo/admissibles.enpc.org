0349a4837594803bba998cf29fc13d2c60368ebc
Thomas <thomasbuiron78@gmail.com>
2021-05-18 21:38:47 +0200

    data base

--- php/conf_server/init.sql
+++ php/conf_server/init.sql
@@ -1,13 +1,24 @@
-CREATE TABLE IF NOT EXISTS `admissibles`.`users` (
+CREATE TABLE IF NOT EXISTS `admissibles`.`eleves` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `prenom` VARCHAR(100) NOT NULL ,
   `nom` VARCHAR(100) NOT NULL ,
   `password` VARCHAR(128) NOT NULL ,
   `mail` VARCHAR(255) NOT NULL ,
-  `mdp_a_changer` BOOLEAN NOT NULL ,
-  `tel` VARCHAR(15) NOT NULL ,
-  `admin` BOOLEAN  DEFAULT FALSE,
-  PRIMARY KEY (`id`))
+  `admin` BOOLEAN  DEFAULT FALSE ,
+  `reservation` BOOLEAN DEFAULT FALSE ,
+  `tel` VARCHAR(15) NOT NULL , PRIMARY KEY (`id`))
   ENGINE = InnoDB;
 
-INSERT INTO `admissibles`.`users` (`id`, `prenom`, `nom`, `password`, `mail`, `mdp_a_changer`, `tel`, `admin`) VALUES ('1', 'admin', 'admin', '$2y$12$F6Q4LapmPtZw8RzpVuAhd.47PJ7KG2xQTUeRB/d3XCI7cjG9qW8OG', 'admin@admissibles.enpc.org', '1', '00000000','1');
+CREATE TABLE IF NOT EXISTS `admissibles`.`chambre` (
+  `numero` INT NOT NULL , 
+  `type` INT NOT NULL , PRIMARY KEY (`numero`)) -- 1 simple, 2 binomee, 3 double
+  ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `admissibles`.`reservation` (
+  `id_eleves` INT NOT NULL ,
+  `numero_chambre` INT NOT NULL ,
+  `date_arrivee` INT NOT NULL ,
+  `date_depart` INT NOT NULL , PRIMARY KEY (`numero`))
+  ENGINE = InnoDB;
+
+  INSERT INTO `admissibles`.`eleves` (`id`, `prenom`, `nom`, `password`, `mail`, `tel`, `admin`, `reservation`) VALUES ('1', 'admin', 'admin', '', 'admin@admissibles.enpc.org', '00000000','1', `TRUE'); --ajouter mot de passe
