596b2552beb5d25319366e0995a2cca1a1dc88eb
bastienlc <bastien.le-chenadec@eleves.enpc.fr>
2021-05-20 12:28:28 +0200

    started mailer.php and trying composer

---
+++ php/mailer.php
@@ -0,0 +1,67 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+//require 'vendor/autoload.php';
+
+function send_mail($email, $activation_code, $prenom) {
+$message= '
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Validation de ton compte Admissibles</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body style="margin: 0; padding: 0;">
+    <p>
+        Bonjour '.$prenom.'.
+        <a href="localhost:8123/activate.php?email='.$email.'&code='.$activation_code.'">Clique ici pour activer ton compte</a>
+    </p>
+</body>
+</html>
+';
+
+$subject = 'Validation de ton compte Admissibles';
+$mail = new PHPMailer(true);
+try {
+    //Server settings
+    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'admissibles.enpc@gmail.com';                     //SMTP username
+    $mail->Password   = getenv("gmail_password");                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('admissibles.enpc@gmail.com', 'Admissibles');
+    $mail->addAddress($email);     //Add a recipient
+    // $mail->addAddress('ellen@example.com');               //Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $subject;
+	   $mail->Body = $message;
+    // $mail->Body    = $message;
+    // $mail->AltBody = $message;
+    $mail->CharSet = "UTF-8";
+
+    $mail->send();
+} catch (Exception $e) {
+
+};
+};
+
+send_mail("bastien.lechenadec@orange.fr", "test", "Bastien");
+
+?>
