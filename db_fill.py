60dd4acfba48b8b546103ab99c3a104b83f008b1
TheiloT <theilo.terrisse@gmail.com>
2021-06-13 19:24:22 +0200

    mise a jour de db_fill.y

--- db_fill.py
+++ db_fill.py
@@ -8,11 +8,13 @@ from datetime import datetime, date, timedelta
 def delete_and_restore_just_admin():
     # Supprime toutes les lignes et remet uniquement la ligne de l'admin dans eleves.
 
+    # Pour savoir quoi renseigner dans "port", faire un "docker ps" et regarder dans "port" pour
+    # l'image "mysql:8.0.20". Prendre celui qui precede "O.O.O.O:"
     connection = mysql.connector.connect(host='localhost',
                                          port=6033,
-                                         database=os.environ['MYSQL_DATABASE'],
-                                         user=os.environ['MYSQL_USER'],
-                                         password=os.environ['MYSQL_PASSWORD'])
+                                         database='admissibles',
+                                         user='root',
+                                         password='NotSecretPassword')
 
     delete_all_chambre = "DELETE FROM chambre;"
     delete_all_demande = "DELETE FROM demande;"
@@ -36,6 +38,9 @@ def delete_and_restore_just_admin():
     connection.commit()
 
 def random_dates():
+    # Genere de facon "coherente" des dates et heures d'arrivee et de depart et une date de realisation de demande,
+    # dans le bon format
+
     # Initializing start date
     beginning_of_inscriptions = date(2021, 6, 14)
 
@@ -62,7 +67,25 @@ def random_dates():
     arr_hour_min = str(arr_hour).rjust(2, "0") + ":" + str(arr_min).rjust(2, "0")
     dep_hour_min = str(dep_hour).rjust(2, "0") + ":" + str(dep_min).rjust(2, "0")
 
-    return(res_arr_date, arr_hour_min, res_dep_date, dep_hour_min)
+    # Date de demande
+    # On doit mettre une date de demande anterieure a la date d'arrivee
+    nb_jours_entre_demande_arrivee = rand.randint(0, 15)
+    demand_date = max(arr_date - timedelta(days=nb_jours_entre_demande_arrivee), beginning_of_inscriptions)
+    if (demand_date == arr_date) :
+        demand_hour = rand.randint(0, arr_hour)
+        if (demand_hour == arr_hour) :
+            demand_min = rand.randint(0, arr_min)
+        else:
+            demand_min = rand.randint(0, 59)
+    else:
+        demand_hour = rand.randint(0, 24)
+        demand_min = rand.randint(0, 59)
+
+    pre_demand_time = datetime(demand_date.year, demand_date.month, demand_date.day, demand_hour, demand_min)
+    demand_time = (pre_demand_time-datetime(1970,1,1)).total_seconds()
+    # La date demande en secondes depuis le 1er janvier 1970
+
+    return(res_arr_date, arr_hour_min, res_dep_date, dep_hour_min, demand_time)
 
 def requetes_chambres(nb):
     liste_queries = []
@@ -130,12 +153,12 @@ def requetes_eleves_demandes(nb_eleves, nb_demandes_simples, nb_demandes_paires)
         type_chambre = rand.randint(1, 3)
         remplace = rand.randint(0, 1)  # Peut-etre plus realiste de mettre une loi binomiale avec p=0.9...
         gender_choice = rand.randint(0, 1)
-        arrival_date, arrival_time, departure_date, departure_time = random_dates()
+        arrival_date, arrival_time, departure_date, departure_time, demand_time = random_dates()
         mate = 0
         mate_email = "NULL"
         validee = 0
-        demande_query="INSERT INTO demande (id_demande, id_eleve, type_chambre, remplace, gender_choice, arrival_date, arrival_time, departure_date, departure_time, mate, mate_email, validee)" \
-                      "VALUES (NULL, %d, %d, %d, %d, '%s', '%s', '%s', '%s', %d, %s, %d);"%(id_eleve, type_chambre, remplace, gender_choice, arrival_date, arrival_time, departure_date, departure_time, mate, mate_email, validee)
+        demande_query="INSERT INTO demande (id_demande, id_eleve, type_chambre, remplace, gender_choice, arrival_date, arrival_time, departure_date, departure_time, mate, mate_email, validee, demand_time)" \
+                      "VALUES (NULL, %d, %d, %d, %d, '%s', '%s', '%s', '%s', %d, %s, %d, %d);"%(id_eleve, type_chambre, remplace, gender_choice, arrival_date, arrival_time, departure_date, departure_time, mate, mate_email, validee, demand_time)
         liste_queries_demande.append(demande_query)
 
     # Les demandes par paires
@@ -147,6 +170,7 @@ def requetes_eleves_demandes(nb_eleves, nb_demandes_simples, nb_demandes_paires)
         # ======
         mail_eleve_1 = liste_eleves[id_eleve_1-2][5]
         genre_eleve_1 = liste_eleves[id_eleve_1-2][3]
+        # Pour rappel, "gender_choice" est le genre avec lequel l'eleve ne veut pas etre, donc on mettre un 1 + %2 pour inverser
 
         # L'eleve 2
         id_eleve_2 = ids.pop()
@@ -159,18 +183,18 @@ def requetes_eleves_demandes(nb_eleves, nb_demandes_simples, nb_demandes_paires)
         # criteres communs aux deux demandes
         type_chambre = rand.randint(1, 3)
         remplace = rand.randint(0, 1)  # Peut-etre pus realiste de mettre une loi binomiale avec p=0.9...
-        arrival_date, arrival_time, departure_date, departure_time = random_dates()  # On fait une simplification ici...
+        arrival_date, arrival_time, departure_date, departure_time, demand_time = random_dates()  # On fait une simplification ici...
         mate = 1
         validee = 0
 
         # La demande de l'eleve 1
-        demande_query="INSERT INTO demande (id_demande, id_eleve, type_chambre, remplace, gender_choice, arrival_date, arrival_time, departure_date, departure_time, mate, mate_email, validee)" \
-                      "VALUES (NULL, %d, %d, %d, %d, '%s', '%s', '%s', '%s', %d, '%s', %d);"%(id_eleve_1, type_chambre, remplace, genre_eleve_2, arrival_date, arrival_time, departure_date, departure_time, mate, mail_eleve_2, validee)
+        demande_query="INSERT INTO demande (id_demande, id_eleve, type_chambre, remplace, gender_choice, arrival_date, arrival_time, departure_date, departure_time, mate, mate_email, validee, demand_time)" \
+                      "VALUES (NULL, %d, %d, %d, %d, '%s', '%s', '%s', '%s', %d, '%s', %d, %d);"%(id_eleve_1, type_chambre, remplace, 1 + genre_eleve_2%2, arrival_date, arrival_time, departure_date, departure_time, mate, mail_eleve_2, validee, demand_time)
         liste_queries_demande.append(demande_query)
 
         # La demande de l'eleve 2
-        demande_query = "INSERT INTO demande (id_demande, id_eleve, type_chambre, remplace, gender_choice, arrival_date, arrival_time, departure_date, departure_time, mate, mate_email, validee)" \
-                        "VALUES (NULL, %d, %d, %d, %d, '%s', '%s', '%s', '%s', %d, '%s', %d);"%(id_eleve_2, type_chambre, remplace, genre_eleve_1, arrival_date, arrival_time, departure_date,departure_time, mate, mail_eleve_1, validee)
+        demande_query = "INSERT INTO demande (id_demande, id_eleve, type_chambre, remplace, gender_choice, arrival_date, arrival_time, departure_date, departure_time, mate, mate_email, validee, demand_time)" \
+                        "VALUES (NULL, %d, %d, %d, %d, '%s', '%s', '%s', '%s', %d, '%s', %d, %d);"%(id_eleve_2, type_chambre, remplace, 1 + genre_eleve_1%2, arrival_date, arrival_time, departure_date,departure_time, mate, mail_eleve_1, validee, demand_time)
         liste_queries_demande.append(demande_query)
 
     for i in range(nb_eleves):
@@ -191,9 +215,9 @@ def generate_lines(nb_chambres, nb_eleves, nb_demandes_simples, nb_demandes_pair
         # l'image "mysql:8.0.20". Prendre celui qui precede "O.O.O.O:"
         connection = mysql.connector.connect(host='localhost',
                                              port=6033,
-                                             database=os.environ['MYSQL_DATABASE'],
-                                             user=os.environ['MYSQL_USER'],
-                                             password=os.environ['MYSQL_PASSWORD'])
+                                             database='admissibles',
+                                             user='root',
+                                             password='NotSecretPassword')
 
         liste_queries_chambre = requetes_chambres(nb_chambres)
         liste_queries_eleve, liste_queries_demande = requetes_eleves_demandes(nb_eleves, nb_demandes_simples, nb_demandes_paires)
@@ -217,4 +241,4 @@ def generate_lines(nb_chambres, nb_eleves, nb_demandes_simples, nb_demandes_pair
             connection.close()
             print("MySQL connection is closed")
 
-generate_lines(10, 10, 4, 2)
+generate_lines(20, 10, 4, 2)
