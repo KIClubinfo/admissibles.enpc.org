8d0a6eff6bb9452737624ce4b16911b5ab313515
Louis-Bly <louis.boilly@gmail.com>
2021-06-15 19:54:09 +0200

    Version alpha: ça marche avec une demande, reste à tester en situation réelle

--- php/attribution.php
+++ php/attribution.php
@@ -49,50 +49,100 @@ $BINOMEE = "2";
 include("config.php");
 include("field_names.php");
 
-$liste_demandes_loin = $con->query("SELECT * FROM eleves JOIN demande ON id = id_eleve WHERE distance > 75 ORDER BY "); //On récupère toutes les demandes triées selon Voisard
-$liste_demandes_pres = $con->query("SELECT * FROM eleves JOIN demande ON id = id_eleve WHERE distance <= 75 ORDER BY boursier DESC, distance DESC"); //On récupère toutes les demandes triées selon Voisard
+$liste_demandes_loin = $con->query("SELECT * FROM eleves JOIN demande ON id = id_eleve WHERE distance > 75 ORDER BY validee DESC, demand_time ASC"); //On récupère toutes les demandes triées selon Voisard
+$liste_demandes_pres = $con->query("SELECT * FROM eleves JOIN demande ON id = id_eleve WHERE distance <= 75 ORDER BY validee DESC, demand_time ASC"); //On récupère toutes les demandes triées selon Voisard
 while($demande = $liste_demandes_loin->fetch_assoc()){   //On regarde 1 à 1 les demandes
-    var_dump($demande);
     $condition_type = " type = ";
     //join pour les binomes
     $voeu1 = $con->query("SELECT * FROM chambre WHERE".$condition_type.$demande[$FIELD_CHAMBRE_TYPE]); //Chambres du bon type (prio)
-    $voeu2 = $con->query("SELECT * FROM chambre WHERE NOT".$condition_type.$demande[$FIELD_CHAMBRE_TYPE]); //Autres chambres
     $cherche = true;
     while($room = $voeu1->fetch_assoc() and $cherche){ //Cycle sur les chambres du bon type -> stock dans $room
         $same_room = $FIELD_CHAMBRE_NUMBER." = ".$room[$FIELD_NUMBER];
         $same_moment = true;
-        var_dump($room);
         $occupe = $con->query("SELECT * FROM reservation JOIN eleves ON id_eleves = id WHERE ".$same_room." AND ".$same_moment);  //Autres locations de cette chambre
         $binome1 = $occupe -> fetch_assoc(); //1ere réservation sur la période
         $binome2 = $occupe -> fetch_assoc(); //2eme reservation sur la période
-        $simple_pleine = $occupe -> num_rows > 0 and $room[$FIELD_TYPE]==0; //simple remplie
+        $simple_pleine = $occupe -> num_rows > 0 and $room[$FIELD_TYPE]==$SIMPLE; //simple remplie
         $deux_personnes = $binome2 != NULL;//deux personnes
-        $incompatible_gender = $binome1[$FIELD_GENDER] == $demande[$FIELD_GENDER]; //Genre incompatible
-        $chambre_valide = !($simple_pleine or $deux_personnes or $incompatible_gender);
+        $incompatible_gender = $binome1 == NULL or $binome1[$FIELD_GENDER] == $demande[$FIELD_GENDER]; //Genre incompatible
+        $chambre_valide = $binome1 == NULL or !($simple_pleine or $deux_personnes or $incompatible_gender);
+        $heure_arrivee = "";
+        for($i=0; $i<strlen($demande[$FIELD_ARRIVAL_TIME])+3; $i++) {
+            if ($i < strlen($demande[$FIELD_ARRIVAL_TIME])) {
+                $heure_arrivee = $heure_arrivee . $demande[$FIELD_ARRIVAL_TIME][$i];
+            }
+            if ($i == 1 or $i == strlen($demande[$FIELD_ARRIVAL_TIME])) {
+                $heure_arrivee = $heure_arrivee . ":";
+            }
+            if($i > strlen($demande[$FIELD_ARRIVAL_TIME])){
+                $heure_arrivee = $heure_arrivee . "0";
+            }
+        }
+        $heure_depart = "";
+        for($i=0; $i<strlen($demande[$FIELD_DEPARTURE_TIME])+3; $i++) {
+            if ($i < strlen($demande[$FIELD_DEPARTURE_TIME])) {
+                $heure_depart = $heure_depart . $demande[$FIELD_DEPARTURE_TIME][$i];
+            }
+            if ($i == 1 or $i == strlen($demande[$FIELD_DEPARTURE_TIME])) {
+                $heure_depart = $heure_depart . ":";
+            }
+            if($i > strlen($demande[$FIELD_DEPARTURE_TIME])){
+                $heure_depart = $heure_depart . "0";
+            }
+        }
         if($chambre_valide){ //Si pas de conflit
-            $con -> query("INSERT INTO reservation (".$FIELD_RESERVATION_ELEVES_ID.",".$FIELD_CHAMBRE_NUMBER.",".$FIELD_DATE_DEPART.",".$FIELD_DATE_ARRIVEE.") 
-            VALUES (".$demande[$FIELD_ID].",".$room[$FIELD_NUMBER].",".$demande[$FIELD_DATE_DEPART].",".$demande[$FIELD_DATE_ARRIVEE].")"); //On réserve la chambre
+            $con -> query("INSERT INTO reservation (".$FIELD_RESERVATION_ELEVES_ID.",".$FIELD_CHAMBRE_NUMBER.",".$FIELD_DATE_ARRIVEE.",".$FIELD_DATE_DEPART.") 
+            VALUES (".$demande[$FIELD_DEMANDE_ELEVE_ID].",".$room[$FIELD_NUMBER].",'".$demande[$FIELD_ARRIVAL_DATE]." ".$heure_arrivee."','".$demande[$FIELD_DEPARTURE_DATE]." ".$heure_depart."')"); //On réserve la chambre
             $cherche = false;//reussi -> affecter
+            $con -> query("DELETE FROM ".$TABLE_DEMANDE." WHERE ".$FIELD_DEMANDE_ELEVE_ID."=".$demande[$FIELD_DEMANDE_ELEVE_ID]);
         }
     }
-    while($room = $voeu2->fetch_assoc() and $cherche) { //Cycle sur les chambres du mauvais type -> stock dans $room
-        $same_room = $FIELD_CHAMBRE_NUMBER." = ".$room[$FIELD_NUMBER];
-        $same_moment = true;
-        $occupe = $con->query("SELECT * FROM reservation JOIN eleves ON id_eleves = id WHERE ".$same_room." AND ".$same_moment);  //Autres locations de cette chambre
-        $binome1 = $occupe -> fetch_assoc(); //1ere réservation sur la période
-        $binome2 = $occupe -> fetch_assoc(); //2eme reservation sur la période
-        $simple_pleine = $occupe -> num_rows > 0 and $room[$FIELD_TYPE]==0; //simple remplie
-        $deux_personnes = $binome2 != NULL;//deux personnes
-        $incompatible_gender = $binome1[$FIELD_GENDER] == $demande[$FIELD_GENDER]; //Genre incompatible
-        $chambre_valide = !($simple_pleine or $deux_personnes or $incompatible_gender);
-        if($chambre_valide){ //Si pas de conflit
-            $con -> query("INSERT INTO reservation (".$FIELD_RESERVATION_ELEVES_ID.",".$FIELD_CHAMBRE_NUMBER.",".$FIELD_DATE_DEPART.",".$FIELD_DATE_ARRIVEE.") 
-            VALUES (".$demande[$FIELD_ID].",".$room[$FIELD_NUMBER].",".$demande[$FIELD_DATE_DEPART].",".$demande[$FIELD_DATE_ARRIVEE].")"); //On réserve la chambre
-            $cherche = false;//reussi -> affecter
+    if($demande[$FIELD_REMPLACE] == 1){
+        $voeu2 = $con->query("SELECT * FROM chambre WHERE NOT".$condition_type.$demande[$FIELD_CHAMBRE_TYPE]); //Autres chambres
+        while($room = $voeu2->fetch_assoc() and $cherche) { //Cycle sur les chambres du mauvais type -> stock dans $room
+            $same_room = $FIELD_CHAMBRE_NUMBER." = ".$room[$FIELD_NUMBER];
+            $same_moment = true;
+            $occupe = $con->query("SELECT * FROM reservation JOIN eleves ON id_eleves = id WHERE ".$same_room." AND ".$same_moment);  //Autres locations de cette chambre
+            $binome1 = $occupe -> fetch_assoc(); //1ere réservation sur la période
+            $binome2 = $occupe -> fetch_assoc(); //2eme reservation sur la période
+            $simple_pleine = $occupe -> num_rows > 0 and $room[$FIELD_TYPE]==$SIMPLE; //simple remplie
+            $deux_personnes = $binome2 != NULL;//deux personnes
+            $incompatible_gender = $binome1 == NULL or $binome1[$FIELD_GENDER] == $demande[$FIELD_GENDER]; //Genre incompatible
+            $chambre_valide = $binome1 == NULL or !($simple_pleine or $deux_personnes or $incompatible_gender);
+            $heure_arrivee = "";
+            for($i=0; $i<strlen($demande[$FIELD_ARRIVAL_TIME])+3; $i++) {
+                if ($i < strlen($demande[$FIELD_ARRIVAL_TIME])) {
+                    $heure_arrivee = $heure_arrivee . $demande[$FIELD_ARRIVAL_TIME][$i];
+                }
+                if ($i == 1 or $i == strlen($demande[$FIELD_ARRIVAL_TIME])) {
+                    $heure_arrivee = $heure_arrivee . ":";
+                }
+                if($i > strlen($demande[$FIELD_ARRIVAL_TIME])){
+                    $heure_arrivee = $heure_arrivee . "0";
+                }
+            }
+            $heure_depart = "";
+            for($i=0; $i<strlen($demande[$FIELD_DEPARTURE_TIME])+3; $i++) {
+                if ($i < strlen($demande[$FIELD_DEPARTURE_TIME])) {
+                    $heure_depart = $heure_depart . $demande[$FIELD_DEPARTURE_TIME][$i];
+                }
+                if ($i == 1 or $i == strlen($demande[$FIELD_DEPARTURE_TIME])) {
+                    $heure_depart = $heure_depart . ":";
+                }
+                if($i > strlen($demande[$FIELD_DEPARTURE_TIME])){
+                    $heure_depart = $heure_depart . "0";
+                }
+            }
+            if($chambre_valide){ //Si pas de conflit
+                $con -> query("INSERT INTO reservation (".$FIELD_RESERVATION_ELEVES_ID.",".$FIELD_CHAMBRE_NUMBER.",".$FIELD_DATE_ARRIVEE.",".$FIELD_DATE_DEPART.") 
+                VALUES (".$demande[$FIELD_DEMANDE_ELEVE_ID].",".$room[$FIELD_NUMBER].",'".$demande[$FIELD_ARRIVAL_DATE]." ".$heure_arrivee."','".$demande[$FIELD_DEPARTURE_DATE]." ".$heure_depart."')"); //On réserve la chambre
+                $cherche = false;//reussi -> affecter
+                $con -> query("DELETE FROM ".$TABLE_DEMANDE." WHERE ".$FIELD_DEMANDE_ELEVE_ID."=".$demande[$FIELD_DEMANDE_ELEVE_ID]);
+            }
         }
     }
     if($cherche){ //si malgré tout on a pas trouvé
-        $con -> query("UPDATE demande SET 'valide' = 3 WHERE 'id_demande' = ".$demande[$FIELD_DEMANDE_ID]); //On place sur liste d'attente
+        $con -> query("UPDATE demande SET validee = 3 WHERE id_demande = ".$demande[$FIELD_DEMANDE_ID]); //On place sur liste d'attente
     }
 }
 ?>
