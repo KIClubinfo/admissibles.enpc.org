99a58369542f1782e8bd713da0a26bc257f1d12a
quentin <malherbe.q@gmail.com>
2024-05-01 18:25:18 +0200

    updated prices

--- php/chambreModaux.php
+++ php/chambreModaux.php
@@ -21,7 +21,7 @@
                             <!-- Portfolio Modal - Image-->
                             <img class="img-fluid rounded mb-5" src="assets/img/simple.jpg" alt="..." />
                             <!-- Portfolio Modal - Text-->
-                            <p class="mb-5 text-secondary">La <strong>surface</strong> de la chambre simple varie entre 18,2 et 24,5 m2. <br/>Chaque chambre inclut une <strong>cuisine</strong>, une <strong>salle de bain</strong> avec <strong>toilettes</strong> et un <strong>bureau</strong>. <br/>Le <strong> tarif </strong> de la chambre est de <strong> 215 € </strong> pour le séjour.</p>
+                            <p class="mb-5 text-secondary">La <strong>surface</strong> de la chambre simple varie entre 18,2 et 24,5 m2. <br/>Chaque chambre inclut une <strong>cuisine</strong>, une <strong>salle de bain</strong> avec <strong>toilettes</strong> et un <strong>bureau</strong>. <br/>Le <strong> tarif </strong> de la chambre est de <strong> 220 € </strong> pour le séjour.</p>
                             <button class="btn btn-primary" data-dismiss="modal">
                                 <i class="fas fa-times fa-fw"></i>
                                 Fermer
@@ -55,7 +55,7 @@
                             <!-- Portfolio Modal - Image-->
                             <img class="img-fluid rounded mb-5" src="assets/img/binome.png" alt="..." />
                             <!-- Portfolio Modal - Text-->
-                            <p class="mb-5 text-secondary">La <strong>surface</strong> de la chambre binômé est d'envrion 20 m2. <br/>Chaque chambre inclut une <strong>cuisine</strong>, une <strong>salle de bain</strong> avec <strong>toilettes</strong> et un <strong>bureau</strong>.<br/> La chambre sera potentiellement<strong> partagée </strong> avec un autre admissible. <br/> Le <strong> tarif </strong> de la chambre est de <strong> 175 € </strong> pour le séjour.</p>
+                            <p class="mb-5 text-secondary">La <strong>surface</strong> de la chambre binômé est d'envrion 20 m2. <br/>Chaque chambre inclut une <strong>cuisine</strong>, une <strong>salle de bain</strong> avec <strong>toilettes</strong> et un <strong>bureau</strong>.<br/> La chambre sera potentiellement<strong> partagée </strong> avec un autre admissible. <br/> Le <strong> tarif </strong> de la chambre est de <strong> 180 € </strong> pour le séjour.</p>
                             <button class="btn btn-primary" data-dismiss="modal">
                                 <i class="fas fa-times fa-fw"></i>
                                 Fermer
@@ -89,7 +89,7 @@
                             <!-- Portfolio Modal - Image-->
                             <img class="img-fluid rounded mb-5" src="assets/img/double.jpg" alt="..." />
                             <!-- Portfolio Modal - Text-->
-                            <p class="mb-5 text-secondary">La <strong>surface</strong> de la chambre double est d'environ 30 m2. <br/>Chaque chambre inclut une <strong>cuisine</strong>, une <strong>salle de bain</strong> avec <strong>toilettes</strong> et un <strong>bureau individuel</strong>. <br/> La chambre sera potentiellement<strong> partagée </strong> avec un autre admissible.</br> Le <strong> tarif </strong> de la chambre est de <strong> 175 € </strong> pour le séjour.</p>
+                            <p class="mb-5 text-secondary">La <strong>surface</strong> de la chambre double est d'environ 30 m2. <br/>Chaque chambre inclut une <strong>cuisine</strong>, une <strong>salle de bain</strong> avec <strong>toilettes</strong> et un <strong>bureau individuel</strong>. <br/> La chambre sera potentiellement<strong> partagée </strong> avec un autre admissible.</br> Le <strong> tarif </strong> de la chambre est de <strong> 180 € </strong> pour le séjour.</p>
                             <button class="btn btn-primary" data-dismiss="modal">
                                 <i class="fas fa-times fa-fw"></i>
                                 Fermer
