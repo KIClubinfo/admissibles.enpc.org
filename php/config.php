e5102792cbc6d6e46f69c5e765f2e7f10374d568
K. Anwar <58338018+anwarden@users.noreply.github.com>
2025-06-09 23:56:10 +0200

    Update config.php pour 2025

--- php/config.php
+++ php/config.php
@@ -10,12 +10,12 @@ if ($debug) {
 $db_password = $_ENV["MYSQL_PASSWORD"];
 $con = new mysqli('db', 'admissibles_user', $db_password, 'admissibles');
 if ($con->connect_error) {
-    header('Location: connexion.php?erreur=bdderror');
+        header('Location: connexion.php?erreur=bdderror');
 	exit();
 }
 
 // email configuration TO MODIFY
-define("SENDGRID_API_KEY", "YOUR API KEY");
+define("SENDGRID_API_KEY", "SG.C7GBSvOpSbGEUVe2B668hg.cEGS-rnWsnzWg30e-F_cBM71hzSVg99sBtFcew6mg2w");
 define("EMAIL_SENDER", "admissibles@enpc.org");
 define("NAME_SENDER", "Logements ENPC admissibles");
 define("EMAIL_REPLY", "noreply@enpc.org");
@@ -32,10 +32,10 @@ function is_admin()
     return False;
 }
 
-$debut_demande=new DateTime('2024-06-11 20:00:00');//à modifier
-$debut_inscription=new DateTime('2024-06-10 18:00:0');//à modifier
-$debut_oraux=new DateTime('2024-06-24');//à modifier
-$fin_oraux=new DateTime('2024-07-19');//à modifier
+$debut_inscription=new DateTime('2025-06-13 18:00:00');//à modifier
+$debut_demande=new DateTime('2025-06-16 10:00:00');//à modifier
+$debut_oraux=new DateTime('2025-06-23');//à modifier
+$fin_oraux=new DateTime('2025-07-2019');//à modifier
 
 function protect($dateprotection) {
     $date = new DateTime(null, new DateTimeZone('Europe/Paris'));
